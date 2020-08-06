0da7a5b06e863020b82ca49e39b6cb69ac90ae52
Jeremy Ellis <keyfreemusic@gmail.com>
2020-08-06 12:54:26 -0700

    Update a02-EloquentTinyML-to-M4.ino

--- m09-Tensoflow/a02-EloquentTinyML-to-M4.ino
+++ m09-Tensoflow/a02-EloquentTinyML-to-M4.ino
@@ -6,19 +6,31 @@
  * 
  * Load to the M7 core to see if it works
  * Serial ploter and LED should work
+ * Note: I re-compile after viewing the serial monitor
+ * before viewing the serial plotter
  * 
  * Then load to M4 Core
  * only the LED should work
  * 
- * will need to upload to the M7 core to load the M4 core
- * The following code in a blank setup
+ * will need to upload the following to the M7 core to load the M4 core
  * 
- * upload bootM4();
+ *  
+ 
+void setup() {
+  #ifdef CORE_CM7  
+     bootM4();
+  #endif
+}
+
+void loop() {
+ 
+}
+
  * 
  * By Jeremy Ellis 
  * Twitter @rocksetta
  * Website https://rocksetta.com
- * created Aug 3rd, 2020
+ * created Aug 6th, 2020
  * 
 */
 #include <EloquentTinyML.h>
@@ -40,25 +52,24 @@ void setup() {
 
 void loop() {
     myCounter +=1;
-    if (myCounter >= 360){myCounter = 0;}
+    if (myCounter >= 360){
+        myCounter = 0;
+    }
     
-    // pick up a random x and predict its sine
-  //  float x = 3.14 * random(100) / 100;
     float x = myCounter * 3.14/180;  // degrees to radians
-   // float x = (float)myCounter;  // degrees to radians
-    float y = sin(myCounter);
+    float y = sin(x);
+    
     float input[1] = { x };
     float predicted = ml.predict(input);
 
+    // presently serial only works on the M7 core
+    Serial.println("sin(" +String(x)+ ") = " + String(y) + "\t predicted: " + String(predicted) );
 
-    Serial.println("sin(" +String(x)+ ") = " +String(y)+ "\t predicted: " +String()+ "predicted" );
-
-    // and y=1 is fully on. The LED's brightness can range from 0-255.
-   // int brightness = (int)(127.5f * (y + 1));
+    // y=1 LED is fully on. The LED's brightness can range from 0-255.
     int brightness = (int)(127.5f * (predicted+1));
 
     // Set the brightness of the LED. If the specified pin does not support PWM,
     // this will result in the LED being on when y > 127, off otherwise.
     analogWrite(5, brightness);  
-    delay(3);
+    delay(3); // slows the process down a bit to see the sine wave
 }
