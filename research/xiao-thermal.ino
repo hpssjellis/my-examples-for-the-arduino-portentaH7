d3f79a79595d07d0636f4c8fa07b611bed495d88
Jeremy Ellis <keyfreemusic@gmail.com>
2020-09-15 09:52:35 -0700

    Update xiao-thermal.ino

--- research/xiao-thermal.ino
+++ research/xiao-thermal.ino
@@ -1,4 +1,23 @@
 /***************************************************************************
+ * 
+ * my14_8x8_thermal_Array.ino
+ * Connections on Portenta H7
+ * GND, 3V3, SCL, SDA connections from Portenta to Thermal array. 
+ * Portenta ---> Thermal Array
+ *    GND   --->   GND
+ *    3V3   --->   VIN
+ *    SCL   --->   SCL
+ *    SDA   --->   SDA
+ * 
+ * Using the AMG8833 IR 88 Thermal Imager Array Temperature Sensor Module 8x8 Infrared Camera Sensor 
+ * From Amazon.com about $52 
+ * 
+ * Sketch by Jeremy Ellis
+ * Twitter @rocksetta
+ * 
+ * 
+ * 
+ * 
   This is a library for the AMG88xx GridEYE 8x8 IR camera
 
   This sketch tries to read the pixels from the sensor
@@ -14,6 +33,7 @@
 
   Written by Dean Miller for Adafruit Industries.
   BSD license, all text above must be included in any redistribution
+  
  ***************************************************************************/
 
 #include <Wire.h>
@@ -23,8 +43,10 @@ Adafruit_AMG88xx amg;
 float pixels[AMG88xx_PIXEL_ARRAY_SIZE];
 bool myFever = false;
 
-float myCalibration = 6.5;   // +- Degrees to calibrate. This might totally depend on your device, may change with time.
+//Calibrate with ICe and hot water that you know the temperature of. Might change with time.
+float myCalibration = 9.4;   // +- Degrees to calibrate. This might totally depend on your device, may change with time.
 float myError = 0.3;         // +- degrees different to print
+float myTempConcern = 39;    // +- Temperature to check. Depends on myCalibration being correct
 float myOldTotal;
 
 void setup() {
@@ -56,7 +78,7 @@ void loop() {
         float myIncoming = pixels[i-1];
         myTotal += myIncoming;
         if (myIncoming > myHighest){myHighest = myIncoming;} // grab the largest
-        if (myIncoming > 31.0){  // raw score for generic uncalibrated face temperature
+        if (myIncoming +myCalibration > myTempConcern){  // raw score for generic uncalibrated face temperature
            if (myTemp1 == 0){ 
              myTemp1 = myIncoming; // allow one false reading
            } else {
@@ -69,9 +91,9 @@ void loop() {
        myFever = true;   // once set don't unset.
       }
 
-    if ( myTotal > myOldTotal + (1*myError*64) || myTotal < myOldTotal + (-1*myError*64) ){  // just checking for change default myError = 0.3    
+    if ( myTotal > myOldTotal + (1*myError*64) || myTotal < myOldTotal + (-1*myError*64) ){  // just checking for change, default myError = 0.3    
          digitalWrite(LED_BUILTIN, LOW);
-         delay(200);      
+         delay(1000);      
          digitalWrite(LED_BUILTIN, HIGH);
          delay(200);      
          Serial.println("8x8 IR array Average Temperature: "+ String(myCalibration+myTotal/64)+ ", Highest Temp: "+String(myCalibration+myHighest));    
@@ -82,12 +104,12 @@ void loop() {
     if (myFever){
       for (int j=0; j<= 60; j++){
          digitalWrite(LED_BUILTIN, LOW);
-         delay(100);
+         delay(50);
          digitalWrite(LED_BUILTIN, HIGH);
-         delay(100);
-         }
-         myFever = false; // reset fever        
-         digitalWrite(LED_BUILTIN, LOW);
+         delay(50);
+       }
+       myFever = false; // reset fever        
+       //digitalWrite(LED_BUILTIN, LOW);   /// why this
       }
 
     //delay a second
