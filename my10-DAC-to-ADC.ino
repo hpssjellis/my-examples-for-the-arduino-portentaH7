20d524e843d239c2ce1b6354895629457efec637
Jeremy Ellis <keyfreemusic@gmail.com>
2020-10-04 08:48:26 -0700

    Update my10-DAC-to-ADC.ino

--- my10-DAC-to-ADC.ino
+++ my10-DAC-to-ADC.ino
@@ -1,12 +1,13 @@
 /* 
+ * my10-DAC-to-ADC.ino
  * DAC to ADC for the PortentaH7
  * Digital to Analog Converter (DAC) and  Analog to Digital Converter (ADC)
  * DAC converts a digital signal into analog signal.
  * ADC converts an Analog signal into a digital signal
  *
  * Connect Pin 3V3 to AREF
- * Connect Pin A6 (DAC) to A0
- * Connect Pin A6 (DAC) to Pin 6 (D6)
+ * Connect Pin A0 (DAC) to A5
+ * Connect Pin A0 (DAC) to Pin 5 (D5)
  *   
  *
  *  Update August 3rd, 2020
@@ -26,11 +27,11 @@
 void setup() {
   
   analogWriteResolution(10);
-  randomSeed(A0);  
+  randomSeed(A1);  // grab a random Analog reading 
   Serial.begin(9600);
   pinMode(DAC, OUTPUT);
-  pinMode(6, INPUT_PULLDOWN);
-  // pinMode(A0, INPUT); You don't have to declare A0 for Analog input, that is the default
+  pinMode(5, INPUT_PULLDOWN);  // set pin D5
+  // pinMode(A5, INPUT); You don't have to declare A6 for Analog input, that is the default
 
 }
 
@@ -41,7 +42,7 @@ void loop() {
   int myRandomNumber  = rand() % 1024;  // a random number between 0 and 1023
   myDac(myRandomNumber);
   Serial.println("----------------------");
-  delay(6000); // wait a few seconds
+  delay(1000); // wait a few seconds
 
 }
 
@@ -49,13 +50,13 @@ void loop() {
 void myDac(int myDacSet){
   
   analogWrite(DAC, myDacSet);
-  Serial.print("DAC set to: ");
+  Serial.print("DAC on A0 set to: ");
   Serial.print(myDacSet);
 
-  Serial.print(", A0: ");
-  Serial.print(analogRead(A0));
+  Serial.print(", A5: ");
+  Serial.print(analogRead(A6));
 
-  Serial.print(", Pin(D6): ");
-  Serial.println(digitalRead(6));
+  Serial.print(", Pin(D5): ");
+  Serial.println(digitalRead(5));
   
 }
