1451028a9532e8b3b0001f5cb9ae54e7186752c1
Jeremy Ellis <keyfreemusic@gmail.com>
2020-12-14 13:39:30 -0800

    Create my15-T32ThreadDebug.ino

---
+++ my15-T32ThreadDebug.ino
@@ -0,0 +1,41 @@
+/*
+  my01b-blink-serial.ino  for the Portenta H7
+
+  Turns on the blue LED on for one second, then off for three seconds, repeatedly.
+  also checks if Serial Printing is working
+
+  July 22nd, 2020
+  by Jeremy Ellis
+  Twitter @rocksetta
+  Website https://www.rocksetta.com
+
+*/
+
+
+#include <ThreadDebug.h>
+
+// Define SerialUSB1 as serial debug port
+// use DEBUG_BREAK_IN_SETUP to break the program at setup() entry, waiting for debugger connection.
+// use DEBUG_NO_BREAK_IN_SETUP to let the program start normally, debugger can attach later.  
+USBSerial SerialUSB1(false, "DebugPort");
+UsbDebugCommInterface  debugComm(&SerialUSB1);
+ThreadDebug            threadDebug(&debugComm, DEBUG_NO_BREAK_IN_SETUP); 
+
+
+int myVar = 6;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LEDB, OUTPUT);   // LEDB = blue, LEDG or LED_BUILTIN = green, LEDR = red (typically for errors)
+
+}
+
+void loop() {
+  Serial.println("Serial print works on the M7 core, just ignored on the M4 Core, unless you use RPC!");
+  digitalWrite(LEDB, LOW);   // Portenta onboard LED connected to 3V3 so ground it to light
+  delay(1000);               // wait for a second
+  myVar += 1;
+  debugBreak();
+  digitalWrite(LEDB, HIGH);  // turn the LED off by not grounding it, weird eh.
+  delay(3000);               // wait for 3 seconds
+}
