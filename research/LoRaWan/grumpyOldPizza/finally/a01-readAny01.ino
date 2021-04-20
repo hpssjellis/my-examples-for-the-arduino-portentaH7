80e90855940d174da3ae0ef587013eca66e5913f
Jeremy Ellis <keyfreemusic@gmail.com>
2021-04-20 15:37:09 -0700

    Create a01-readAny01.ino

---
+++ research/LoRaWan/grumpyOldPizza/finally/a01-readAny01.ino
@@ -0,0 +1,39 @@
+
+#include "LoRaRadio.h"
+
+static void myMasterReceiveCallback(void);
+
+
+static void myReceive(void){
+
+   for (int i = 0; i < LoRaRadio.parsePacket(); i++) {
+        Serial.print((char)LoRaRadio.read());
+   } 
+   Serial.println("");
+  
+   LoRaRadio.receive(5);
+}
+
+
+void setup( void ){
+    Serial.begin(9600);
+    
+    while (!Serial) { }   // non-blocking for the murata module on the Portenta
+
+    LoRaRadio.begin(915000000);
+
+    LoRaRadio.setFrequency(915000000);
+    LoRaRadio.setTxPower(1);    //default 14
+    LoRaRadio.setBandwidth(LoRaRadio.BW_125);
+    LoRaRadio.setSpreadingFactor(LoRaRadio.SF_7);
+    LoRaRadio.setCodingRate(LoRaRadio.CR_4_5);
+    LoRaRadio.setLnaBoost(true);
+  
+    LoRaRadio.onReceive(myReceive);  .. just telling it about the callback 
+    LoRaRadio.receive(5);    // not much of a wait between receives
+
+}
+
+void loop( void ){
+
+}
