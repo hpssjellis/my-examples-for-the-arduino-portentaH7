2e2cf0c61858f0e1ff7bc171d39868ff199e7e50
Jeremy Ellis <keyfreemusic@gmail.com>
2021-04-21 22:55:43 -0700

    Create a01-testingA.ino

---
+++ research/LoRaWan/grumpyOldPizza/finally/a01-testingA.ino
@@ -0,0 +1,55 @@
+#include "LoRaRadio.h"
+
+void setup( void )
+{
+    Serial.begin(9600);
+    
+    while (!Serial) { }  // non-blocking on Portenta murata module
+
+    LoRaRadio.begin(915000000);
+
+    LoRaRadio.setFrequency(915000000);
+    LoRaRadio.setTxPower(1);    //default 14
+    LoRaRadio.setBandwidth(LoRaRadio.BW_125);
+    LoRaRadio.setSpreadingFactor(LoRaRadio.SF_7);
+    LoRaRadio.setCodingRate(LoRaRadio.CR_4_5);
+    LoRaRadio.setLnaBoost(true);
+
+}
+
+void loop( void ){
+   // LoRaRadio.beginPacket();
+   //    LoRaRadio.write('P');
+   //    LoRaRadio.write('O');
+   //    LoRaRadio.write('N');
+   //    LoRaRadio.write('G');
+   //    LoRaRadio.endPacket();
+    delay(10000);              // wait x seconds
+
+ //String msg = Serial.readStringUntil('\n');
+ //String msg = "Hello-Lora";
+ char msg[25] = "Hello-Lora-Cool-12345678";
+
+
+  Serial.println();
+  //Serial.print("Sending: " + msg + " - ");
+  //for (unsigned int i = 0; i < msg.length(); i++) {
+  //  Serial.print(msg[i] >> 4, HEX);
+  //  Serial.print(msg[i] & 0xF, HEX);
+   // Serial.print(" ");
+  //}
+  Serial.println();
+
+  int err;
+  LoRaRadio.beginPacket();
+ // LoRaRadio.print(msg);    
+  LoRaRadio.write(msg, sizeof(msg));    
+  LoRaRadio.endPacket(); 
+
+  Serial.println("Message sent: "+String(msg));
+
+
+
+
+    
+}
