634c21a7d40d312d747ddf1c8c1b1d65bbdd3c50
Jeremy Ellis <keyfreemusic@gmail.com>
2021-11-29 10:56:46 -0800

    Create RAM-weird-06.ino

---
+++ research/RAM-m4-m7/RAM-weird-06.ino
@@ -0,0 +1,152 @@
+
+// Original by Khoi Hoang (Github @khoih-prog ) and Jeremy Ellis (Github @hpssjellis )
+// Note: each core can be seperated to it's own IDE folder
+
+#ifdef CORE_CM7    // Start M7 Core programming
+
+#include "mbed.h"
+#include "Arduino.h"
+
+using namespace mbed;
+using namespace rtos;
+
+//#define SRAM4_START_ADDRESS       ((uint32_t) 0x30040000)   
+#define SRAM4_START_ADDRESS       ((uint32_t) 0x38000000)
+
+struct shared_data{
+  int M4toM7;
+  int M7toM4;
+};
+
+int localm7m4 = 0;
+int localm4m7 = 0;
+unsigned long myDelay = 43210;  // microSeconds not milliSeconds
+unsigned long delayStart = 0;  
+
+
+// Using AHB SRAM4 at 0x38000000
+static struct shared_data * const xfr_ptr = (struct shared_data *) SRAM4_START_ADDRESS;
+
+
+
+Thread M7Thread;
+
+void setup(){
+  bootM4();
+
+  Serial.begin(115200);
+  //yield();
+  delay(10);  // give M4 time to boot!
+  yield();
+
+  //while (!Serial);  // still blocking when removed? 
+  
+  
+  //  WEIRD, THESE 3 LINES ARE NEEDED FOR THE m4 CORE TO UPDATE THE VARIABLE
+  Serial.println(F("\nStarting M7_M4_sharemem_SRAM on Portenta_H7"));
+  Serial.print(F("Using SRAM4 @ 0x"));  
+  Serial.println(SRAM4_START_ADDRESS, HEX);
+ 
+  M7Thread.start(callback(M7ThreadFunc));
+  yield();
+}
+
+void M7ThreadFunc(){
+  localm7m4 = 777777;
+  
+  while (true) {  
+   if ( (micros() - delayStart ) >= myDelay) {
+    delayStart = micros(); //  reset the delay time 
+    yield();
+    localm7m4++;
+    xfr_ptr -> M7toM4 = localm7m4;
+    
+    Serial.print("M7 to M4: "); Serial.println(xfr_ptr -> M7toM4);
+    Serial.print("M4 to M7: "); Serial.println(xfr_ptr -> M4toM7);
+
+    Serial.println();        
+    yield();
+    //delay(20);
+    
+
+   //delayMicroseconds(43210);    // Note: micro-seconds not milli-seconds
+  }
+ }
+}
+
+void loop(){
+  yield();
+}
+
+#endif
+
+//////////////////////////////////////////////////////////
+
+
+
+
+#ifdef CORE_CM4    // Start M4 Core programming
+
+
+#include "mbed.h"
+#include "Arduino.h"
+
+using namespace mbed;
+using namespace rtos;
+
+
+//#define SRAM4_START_ADDRESS       ((uint32_t) 0x30040000)   
+#define SRAM4_START_ADDRESS       ((uint32_t) 0x38000000)
+
+struct shared_data{
+  int M4toM7;
+  int M7toM4;
+};
+
+int localm7m4 = 0;
+int localm4m7 = 0;
+unsigned long myDelay = 43210;  // microSeconds not milliSeconds
+unsigned long delayStart = 0;  
+
+
+static struct shared_data * const xfr_ptr = (struct shared_data *) SRAM4_START_ADDRESS;
+
+
+int myStoreFromM7;
+
+Thread M4Thread;
+
+void setup(){
+  pinMode(LEDB, OUTPUT);  // portenta blue LED
+  yield();
+  M4Thread.start(callback(M4ThreadFunc));
+  yield();
+}
+
+void M4ThreadFunc(){
+  localm4m7 = 44444444;
+  
+  while (true){
+    if ( (micros() - delayStart ) >= myDelay) {
+    delayStart = micros(); //  reset the delay time
+    localm4m7++;
+ 
+    xfr_ptr -> M4toM7 = localm4m7;
+
+    // test if M4 an read struct changed on M7 
+    if (myStoreFromM7 !=  xfr_ptr -> M7toM4){
+        myStoreFromM7 =  xfr_ptr -> M7toM4;
+        digitalWrite(LEDB, !digitalRead(LEDB)); // flip blue LED on and off
+     }
+     yield();
+    // delay(20);  
+   // delayMicroseconds(43210); //Note: micro-seconds not milli-seconds
+  }
+ }
+}
+
+void loop(){
+  yield();
+}
+
+#endif
